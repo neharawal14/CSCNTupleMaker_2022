b315266f430c4504bd0d66ff810e2a9d8979cb3b
Matthew Snowball <snowball@cern.ch>
2014-02-24 10:18:41 -0500

    first commit

---
+++ UFCSCRootMaker/test/UFCSCRootMaker.py
@@ -0,0 +1,175 @@
+######################################################################
+#                                                                    #
+# This version works with CMSSW_6_2_X                                #
+#                                                                    #
+######################################################################
+import FWCore.ParameterSet.Config as cms
+
+########## Options ############
+isDATA = bool(False)
+isRAW = bool(True)
+isDIGI = bool(True)
+isSIM = bool(True)
+isGEN = bool(True)
+isLocalRECO = bool(True)
+isFullRECO = bool(False)
+
+addMuonInfo = bool(True)
+addTrackInfo = bool(True)
+addRecHitInfo = bool(True)
+addSegmentInfo = bool(True)
+addTriggerInfo = bool(True)
+addDigiInfo = bool(True)
+addTimeMonitoringInfo = bool(True)
+addCalibrationInfo = bool(True)
+
+maxEvents = 10
+
+MCGlobalTag = "POSTLS162_V2::All"
+DataGlobalTag = "FT_53_V21_AN3::All"
+
+doDebug = bool(False)
+###############################
+
+### Debug Printing ###
+if not isDATA :
+    print "Sample Type: MC"
+else :
+    print "Sample Type: Data"
+                
+#####################
+process = cms.Process("UFCSCRootMaker")
+
+process.Timing = cms.Service("Timing",
+                             summaryOnly = cms.untracked.bool(True)
+                             )
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(maxEvents))
+
+# initialize MessageLogger and output report
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.MessageLogger.cerr.threshold = 'ERROR' # Options: INFO, WARNING, ERROR
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.MessageLogger.destinations = ['cout', 'cerr']
+process.MessageLogger.suppressWarning.append('classByHitsGlb') # kill stupid RPC hit associator warning
+#process.MessageLogger.cerr.FwkJob.limit=1
+#process.MessageLogger.cerr.ERROR = cms.untracked.PSet( limit = cms.untracked.int32(1))
+                                                       
+######################### Frontier Conditions #########################
+# Conditions data for calibration and alignment                       #
+# are defined in the Offline Conditions Database (ORCOF),             #
+# which is read in CMSSW applications via Frontier caching servers.   #
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions  #
+#######################################################################
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+
+if not isDATA:
+    process.GlobalTag.globaltag=MCGlobalTag 
+    
+else:
+    process.GlobalTag.globaltag=DataGlobalTag
+
+
+#process.load("RecoLocalCalo.EcalRecAlgos.EcalSeverityLevelESProducer_cfi")
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               #fileName = cms.untracked.string('test.root'),
+                               # save only events passing the full path
+                               outputCommands = cms.untracked.vstring('drop *')
+                               )
+
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("cscRootMaker.root")
+                                   )
+
+# Primary Vertices
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter("VertexSelector",
+                                                  src = cms.InputTag('offlinePrimaryVertices'),
+                                                  cut = cms.string('!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2'),
+                                                  filter = cms.bool(True)
+                                                  )
+
+
+process.source = cms.Source ("PoolSource",
+                             # Disable duplicate event check mode because the run and event -numbers
+                             # are incorrect in current Madgraph samples (Dec 16, 2008)
+                             duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+                             fileNames = cms.untracked.vstring(),      
+                             )
+
+if isDATA:
+    process.source.fileNames = cms.untracked.vstring(
+
+    
+        )
+else:
+    process.source.fileNames = cms.untracked.vstring(
+        'file:../../../RecoLocalMuon/CSCValidation/test/RelValJpsiMM_GEN-SIM-DIGI-RAW-HLTDEBUG_PRE_ST62_V8-v1/RECO.root'
+        )
+    
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(False)
+                                     #,SkipEvent = cms.untracked.vstring('ProductNotFound')
+                                     )
+
+### Physics Declared Filter (for data)
+process.load('HLTrigger.special.hltPhysicsDeclared_cfi')
+process.hltPhysicsDeclared.L1GtReadoutRecordTag = 'gtDigis'
+
+### No scraping
+process.noscraping = cms.EDFilter("FilterOutScraping",
+                                  applyfilter = cms.untracked.bool(True),
+                                  debugOn = cms.untracked.bool(doDebug),
+                                  numtrack = cms.untracked.uint32(10),
+                                  thresh = cms.untracked.double(0.2)
+                                  )
+
+# HB + HE noise filtering
+process.load('CommonTools.RecoAlgos.HBHENoiseFilter_cfi')
+process.HBHENoiseFilter.minIsolatedNoiseSumE        = 999999.
+process.HBHENoiseFilter.minNumIsolatedNoiseChannels = 999999
+process.HBHENoiseFilter.minIsolatedNoiseSumEt       = 999999.
+
+#EventCount                                                                                                                             
+process.nEventsTotal = cms.EDProducer("EventCountProducer")
+
+#RM
+process.load("UFCSCSoftware.UFCSCRootMaker.cscRootMaker_cfi")
+process.cscRootMaker.isFullRECO = cms.untracked.bool(isFullRECO)
+process.cscRootMaker.isLocalRECO = cms.untracked.bool(isLocalRECO)
+process.cscRootMaker.isGEN = cms.untracked.bool(isGEN)
+process.cscRootMaker.isSIM = cms.untracked.bool(isSIM)
+process.cscRootMaker.isRAW = cms.untracked.bool(isRAW)
+process.cscRootMaker.isDIGI = cms.untracked.bool(isDIGI)
+process.cscRootMaker.isDATA = cms.untracked.bool(isDATA)
+process.cscRootMaker.addMuons = cms.untracked.bool(addMuonInfo)
+process.cscRootMaker.addTracks = cms.untracked.bool(addTrackInfo)
+process.cscRootMaker.addRecHits = cms.untracked.bool(addRecHitInfo)
+process.cscRootMaker.addSegments = cms.untracked.bool(addSegmentInfo)
+process.cscRootMaker.addTrigger = cms.untracked.bool(addTriggerInfo)
+process.cscRootMaker.addDigis = cms.untracked.bool(addDigiInfo)
+process.cscRootMaker.addTimeMonitoring = cms.untracked.bool(addTimeMonitoringInfo)
+process.cscRootMaker.addCalibrations = cms.untracked.bool(addCalibrationInfo)
+
+
+process.p = cms.Path(
+    process.nEventsTotal
+#    *process.goodOfflinePrimaryVertices
+#    *process.noscraping
+    *process.cscRootMaker
+    )
+
+if not isDATA:
+    process.load("GeneratorInterface.GenFilters.TotalKinematicsFilter_cfi")
+    process.totalKinematicsFilter.tolerance=5.0
+    process.p.replace(process.nEventsTotal,process.nEventsTotal*process.totalKinematicsFilter)
+
+if isDATA:
+    process.p.replace(process.nEventsTotal,process.hltPhysicsDeclared*process.nEventsTotal)
