77d615bddbf2a3243eabc3d62bab067907a5feee
neha rawal <nrawal@lxplus.cern.ch>
2025-05-30 17:31:07 +0200

    Modifying global tag for the template in 2018 and some more debug flag in the framework

--- UFCSCRootMaker/test/UFCSCRootMaker_template.py
+++ UFCSCRootMaker/test/UFCSCRootMaker_template.py
@@ -28,7 +28,7 @@ addDigiInfo = bool(True)
 addTimeMonitoringInfo = bool(True)
 addCalibrationInfo = bool(True)
 
-yearInfo = 2017
+yearInfo = 2016
 
 maxEvents = -1
 
@@ -36,9 +36,9 @@ maxEvents = -1
 #DataGlobalTag='76X_dataRun2_v19'
 #DataGlobalTag='76X_dataRun2_v15'
 # 2016 
-#DataGlobalTag='106X_dataRun2_v27'
+DataGlobalTag='106X_dataRun2_v27'
 # 2017
-DataGlobalTag='106X_dataRun2_v20'
+#DataGlobalTag='106X_dataRun2_v20'
 # 2018
 #DataGlobalTag='106X_dataRun2_v24'
 
@@ -145,9 +145,13 @@ process.source = cms.Source ("PoolSource",
 if isDATA:
     process.source.fileNames = cms.untracked.vstring(
         # This job failed, but has output with 3155 entries : 2018A : SingleMuon_Run2018A-ZMu-12Nov2019_UL2018_rsb-v1_1615.root 
-#        'root://cmsxrootd.fnal.gov//store/data/Run2018A/SingleMuon/RAW-RECO/ZMu-12Nov2019_UL2018_rsb-v1/240002/F27FD65D-3263-484D-8DDC-EB5A9B7669F6.root',
         # This job failed, but has output with 3422 entries : 2018D : SingleMuon_Run2018D-ZMu-12Nov2019_UL2018-v4_1366.root
-        'root://cmsxrootd.fnal.gov///store/data/Run2017F/SingleMuon/RAW-RECO/ZMu-09Aug2019_UL2017-v1/130000/002852F9-ACBE-3648-87C5-C4C6417FEE25.root',
+        #'root://cmsxrootd.fnal.gov///store/data/Run2017F/SingleMuon/RAW-RECO/ZMu-09Aug2019_UL2017-v1/130000/002852F9-ACBE-3648-87C5-C4C6417FEE25.root',
+         'root://cmsxrootd.fnal.gov///store/data/Run2016H/SingleMuon/RAW-RECO/ZMu-21Feb2020_UL2016-v1/250000/FB29AD8B-B9D4-BE46-8D33-FE755413238C.root',
+#        'root://cmsxrootd.fnal.gov///store/data/Run2016H/SingleMuon/RAW-RECO/ZMu-21Feb2020_UL2016-v1/250000/FB34284F-B08B-E74C-81C1-693D08AFD3DA.root',
+#        'root://cmsxrootd.fnal.gov///store/data/Run2016H/SingleMuon/RAW-RECO/ZMu-21Feb2020_UL2016-v1/250000/FB422B84-B5E7-8343-BF93-A7F9B52C69DE.root',
+#        'root://cmsxrootd.fnal.gov///store/data/Run2016H/SingleMuon/RAW-RECO/ZMu-21Feb2020_UL2016-v1/250000/FB784FEF-1715-C240-851B-B23BCE6658B9.root',
+#        'root://cmsxrootd.fnal.gov///store/data/Run2016H/SingleMuon/RAW-RECO/ZMu-21Feb2020_UL2016-v1/250000/FC1D2E95-2D16-2948-994A-6A3751640DEC.root',
         #'root://cmsxrootd.fnal.gov//store/data/Run2018D/SingleMuon/RAW-RECO/ZMu-12Nov2019_UL2018-v4/100000/7081838A-B066-9748-A4A8-5752A05AB332.root',
         #    'root://cmsxrootd.fnal.gov///store/data/Run2018C/SingleMuon/RAW-RECO/ZMu-12Nov2019_UL2018-v2/100000/002BB422-02B7-704F-B2CF-4724E9C1F537.root',
         #'root://cmsxrootd.fnal.gov///store/data/Run2022C/SingleMuon/RAW-RECO/ZMu-PromptReco-v1/000/355/872/00000/02f04890-4813-4992-b995-1e08f4905ab1.root', 
